808f69c8a94fd4f5e5916e70bab384caa2cdc2c3
Rotset <magaliga38@gmail.com>
2022-08-10 17:31:23 -0500

    cambios

---
+++ lib/src/environment/environment.dart
@@ -0,0 +1,10 @@
+class Environment {
+
+  static const String API_URL  = "http://192.168.56.1:3000/";
+  static const String API_MERCADO_PAGO  = "https://api.mercadopago.com/v1";
+  static const String API_URL_OLD  = "192.168.56.1:3000";
+  static const String API_KEY_MAPS  = "TU_API_KEY_DE_GOOGLE_MAPS";
+  static const String ACCESS_TOKEN  = "TU_ACCESS_TOKEN_DE_MERCADO_PAGO";
+  static const String PUBLIC_KEY  = "TU_PUBLIC_KEY_DE_MERCADO_PAGO";
+
+}
